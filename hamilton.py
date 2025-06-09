9b0e7d17d60c921118fe6a08f507e0f739665bac
Jeff Levy <levyjeff@hotmail.com>
2025-06-09 14:22:34 -0500

    Fixed concat issue, added HP to example

--- hamilton.py
+++ hamilton.py
@@ -26,7 +26,7 @@ def hamilton_filter(data, h=8, p=4):
         trend.name = f'{data.name}.trend'
         rand.name = f'{data.name}.rand'
         #puts the correct NaNs into the series
-        d = pd.concat([data, cycle, trend, rand])
+        d = pd.concat([data, cycle, trend, rand], axis=1)
         cycle, trend, rand = d[f'{data.name}.cycle'], d[f'{data.name}.trend'], d[f'{data.name}.rand']
     else:
         cycle = res.resid_pearson
@@ -37,14 +37,23 @@ def hamilton_filter(data, h=8, p=4):
 if __name__ == '__main__':
     import pandas as pd
     import matplotlib.pyplot as plt
+    from statsmodels.tsa.filters.hp_filter import hpfilter
 
-    gdpc1 = pd.read_csv('gdpc1.csv', parse_dates=['DATE'], index_col='DATE')
+    gdpc1 = pd.read_csv(r'data\gdpc1.csv', parse_dates=['DATE'], index_col='DATE')
     gdpc1['GDPC1'] = 100*np.log(gdpc1['GDPC1'])
     c, t, r = hamilton_filter(gdpc1['GDPC1'], h=8, p=4)
     df = pd.concat([gdpc1, t, c, r], axis=1)
 
-    fig, axs = plt.subplots(2, 1, figsize=(12,6))
-    axs[0].plot(c.index, c.values, 'b-', label=c.name)
-    axs[1].plot(t.index, t.values, 'r-', label=t.name)
-    fig.legend(ncols=2, loc='upper center')
+    hp_c, hp_t = hpfilter(gdpc1['GDPC1'], lamb=129600)
+
+    fig, axs = plt.subplots(3, 1, figsize=(12,6), sharex=True)
+    axs[0].plot(gdpc1.index, gdpc1['GDPC1'], 'k-')
+    axs[1].plot(c.index, c.values, 'b-', label='Hamilton Filter')
+    axs[1].plot(hp_c.index, hp_c.values, 'r-', label='HP Filter')
+    axs[2].plot(t.index, t.values, 'b-')
+    axs[2].plot(hp_t.index, hp_t.values, 'r-')
+    axs[0].set_ylabel('GDPC1')
+    axs[1].set_ylabel('Cycle')
+    axs[2].set_ylabel('Trend')
+    fig.legend(ncols=3, loc='upper center')
     plt.show()
